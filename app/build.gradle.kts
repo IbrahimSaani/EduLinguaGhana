14ce1a5b5143a2fea56d2c28f0719fe4cb64ce49
IBRAHIM SAANI <ibrahimsaani41@gmail.com>
2026-05-25 20:06:08 -1200

    chore(config): bump version code and name to 1.0.3
    This commit updates the application versioning configuration to prepare for a new release.
    
    - **chore(config):** Update versioning in `app/build.gradle.kts`.
        - Incremented `versionCode` from `3` to `4`.
        - Updated `versionName` from `1.0.2` to `1.0.3`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.edulinguaghana"
         minSdk = 23
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
